8215b9d3e4a91370a39ec2edded82a6f6f7e84fe
Bastien <baztoune@gmail.com>
2011-09-17 01:06:22 +0200

    Added categories and ribbon

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 /* Author: 
 
 */
-
+$(document).ready(function(){
+	$('#gimmeMoreSection').hide();
+	$('#gimmeMore').click(function(e){
+		$(e.target).remove();
+		$('#gimmeMoreSection').show(1000);
+	});
+});
 
 
 
