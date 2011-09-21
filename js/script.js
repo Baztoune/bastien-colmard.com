2dadf98f3fd7ae49cd53fa1dc6ab217898e06623
Bastien <baztoune@gmail.com>
2011-09-21 21:12:58 +0200

    quicksand works

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 /* Author: 
 
 */
+
 $(document).ready(function(){
 	$('#categorySelector').hide();
-	$('#extendedDesc a').hide();
+	$('#extendedDesc').hide();
 	
 	$('#gimmeMore').click(function(e){
 		$(e.target).hide();
 		$('#categorySelector').show();
 		
-		$('#extendedDesc a').show(1000);
+		$('#webItemsContainer').quicksand($('#extendedDesc a'));
+	});
+
+	$('#extendedDesc a').each(function(i){
+		$(this).attr('data-id',i);
 	});
 	
-
-	
-
+	$("#categorySelector li").click(function(elm){
+		$('#webItemsContainer').quicksand(
+			$('#extendedDesc').clone()
+				.find('a[data-tags~="' + $(elm.target).attr('data-id')+ '"]')
+		);
+	})
 });
 
