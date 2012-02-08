9b5fac84d47c6cf85b67566e6c3ef89d8fc677ad
Bastien <baztoune@gmail.com>
2012-02-08 23:18:15 +0100

    Remove useless

--- js/script.js
+++ js/script.js
@@ -3,25 +3,6 @@
 */
 
 $(document).ready(function(){
-	$('#categorySelector').hide();
-	$('#extendedDesc').hide();
 	
-	$('#gimmeMore').click(function(e){
-		$(e.target).hide();
-		$('#categorySelector').show();
-		
-		$('#webItemsContainer').quicksand($('#extendedDesc a'));
-	});
-
-	$('#extendedDesc a').each(function(i){
-		$(this).attr('data-id',i);
-	});
-	
-	$("#categorySelector li").click(function(elm){
-		$('#webItemsContainer').quicksand(
-			$('#extendedDesc').clone()
-				.find('a[data-tags~="' + $(elm.target).attr('data-id')+ '"]')
-		);
-	})
 });
 
