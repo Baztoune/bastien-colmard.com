aa7e88e323ce7bdebbc5717914aff7b36dce21ed
Bastien Colmard <baztoune@gmail.com>
2013-02-09 00:17:18 +0100

    Added v2

---
+++ v2/gallery/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Gallery</title>
+	</head>
+	
+	<body style="background:black;text-align:center">
+		<iframe align="center" src="http://www.flickr.com/slideShow/index.gne?user_id=39357410@N08" frameBorder="0" width="1000" height="750" scrolling="no"></iframe>
+	</body>
+</html>
---
+++ v2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="refresh" content="0; url=en/" />
+	<title>Redirection</title>
+	<meta name="robots" content="noindex,follow" />
+</head>
+	<body>
+	<p><a href="/en/">Redirection</a></p>
+	</body>
+
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	try {
+	var pageTracker = _gat._getTracker("UA-2317291-8");
+	pageTracker._setDomainName(".bastien-colmard.com");
+	pageTracker._trackPageview();
+	} catch(err) {}</script>
+</html>
